04fd90317feb08f79052a19ca98820247ca14304
Bakir Mujkanovic <bakir.mujkanovic01@gmail.com>
2021-09-18 22:16:15 +0200

    feature: first release

---
+++ index.js
@@ -0,0 +1,173 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+const Discord = require("discord.js");
+const Distube = require("distube");
+const prefix = "?";
+const token = process.env["TOKEN"];
+const client = new Discord.Client({
+  restTimeOffset: 0, // reaction speed fastest
+  presence: {
+    status: "dnd",
+    // activity: {
+    //   name: "Custom music bot for Discord",
+    //   type: "PLAYING",
+    // },
+  },
+});
+
+const distube = new Distube(client, {
+  searchSongs: false,
+  emitNewSongOnly: false,
+  highWaterMark: 1024 * 1024 * 64,
+  leaveOnEmpty: true,
+  leaveOnFinish: true,
+  youtubeDL: true,
+  updateYoutubeDL: true,
+});
+
+client.login(token);
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} Bot is online!`);
+});
+
+client.on("message", (message) => {
+  if (
+    !message.content.startsWith(prefix) ||
+    !message.guild ||
+    message.author.bot
+  )
+    return;
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift();
+
+  if (command === "ping") return message.reply(`pong ${client.ws.ping}ms`);
+  else if (command === "play" || command === "p") {
+    message.reply("Searching..");
+    distube.play(message, args.join(" "));
+  } else if (command === "stop") {
+    const queue = distube.getQueue(message);
+    if (!queue) return;
+    distube.stop(message);
+    message.reply("Music stopped");
+  } else if (command === "skip") {
+    const queue = distube.getQueue(message);
+    if (!queue) return;
+    distube.skip(message);
+    message.reply("Song skipped");
+  } else if (command === "autoplay") {
+    let mode = distube.toggleAutoplay(message);
+    message.channel.send(
+      "Set autoplay mode to `" + (mode ? "On" : "Off") + "`"
+    );
+  } else if (command === "loop") {
+    if (!(args[0] !== "on" || args[0] !== "off")) return;
+    let mode =
+      args[0] === "off"
+        ? distube.setRepeatMode(message, 0)
+        : distube.setRepeatMode(message, 1);
+    message.channel.send(`Set repeat mode to ${mode === 0 ? "off" : "on"}.`);
+  } else if (command === "current") {
+    const queue = distube.getQueue(message);
+    if (!queue) return;
+    const song = queue.songs[0];
+    message.channel.send(
+      `Playing \`${song.name}\` - \`[${song.formattedDuration}](${
+        song.url
+      })\`\nRequested by: ${song.user}\n${stts(queue)}`
+    );
+  } else if (command === "queue") {
+    let queue = distube.getQueue(message);
+    message.channel.send(
+      "Current queue:\n" +
+        queue.songs
+          .map(
+            (song, id) =>
+              `**${id + 1}**. [${song.name}] - \`${song.formattedDuration}\``
+          )
+          .join("\n")
+    );
+  } else if (command === "jump") {
+    const num = parseInt(args[0]) - 1;
+    let queue = distube.getQueue(message);
+    if (!queue) {
+      message.reply("No songs in queue");
+      return;
+    }
+    if (num > queue.songs.length || num < 1) {
+      message.reply("Invalid Song");
+      return;
+    }
+    distube.jump(message, num);
+    message.reply(`Jump to ${num + 1} in queue`);
+  } else if (command === "help" || command === "h") {
+    message.reply(
+      `Current commands:\np|play {name|link} - Play the specified song  :arrow_forward:\nskip - Skip current song  :track_next:\nstop - Stop the music :stop_button:\ncurrent - Information about the song currently playing  :musical_note:\nqueue - Information about the queue  :notes:\njump {number} - Play song in specified queue position  :eject:\nautoplay - Toggle autoplay(default: on)  :repeat_one:\nloop {on|off} - Set loop to on|off(default: off)  :arrows_counterclockwise:\ninfo - Information about the bot  :information_source:`
+    );
+  } else if (command === "info") {
+    message.reply(
+      `Custom built music bot for Discord.\nType *?help* for more commands. If you want a certain command implemented, message @muke.\nCurrent version: v1.0.3`
+    );
+  } else {
+    message.reply("Unknown command");
+  }
+});
+
+// Queue status template
+const stts = (queue) =>
+  `Volume: \`${queue.volume}%\` | Filter: \`${
+    queue.filter || "Off"
+  }\` | Loop: \`${queue.repeatMode === 0 ? "Off" : "On"}\` | Autoplay: \`${
+    queue.autoplay ? "On" : "Off"
+  }\``;
+
+// DisTube event listeners, more in the documentation page
+distube
+  //ONCE A SONG STARTS PLAYING SEND INFORMATIONAL MESSAGE
+  .on("playSong", (message, queue, song) =>
+    message.channel.send(
+      `Playing \`${song.name}\` - \`[${song.formattedDuration}](${
+        song.url
+      })\`\nRequested by: ${song.user}\n${stts(queue)}`
+    )
+  )
+  //ONCE A SONG IS ADDED TO THE QUEUE SEND INFORMATIONAL MESSAGE
+  .on("addSong", (message, queue, song) =>
+    message.channel.send(
+      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    )
+  )
+  //ONCE A PLAYLIST STARTS PLAYING SEND INFORMATIONAL MESSAGE
+  .on("playList", (message, queue, playlist, song) =>
+    message.channel.send(
+      `Play \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs).\nRequested by: ${song.user}\nNow playing \`${song.name}\` - \`${
+        song.formattedDuration
+      }\`\n${stts(queue)}`
+    )
+  )
+  //ONCE A PLAYLIST IS ADDED TO THE QUEUE SEND INFORMATIONAL MESSAGE
+  .on("addList", (message, queue, playlist) =>
+    message.channel.send(
+      `Added \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs) to queue\n${stts(queue)}`
+    )
+  )
+  // DisTubeOptions.searchSongs = true
+  .on("searchResult", (message, result) => {
+    let i = 0;
+    message.channel.send(
+      `**Choose an option from below**\n${result
+        .map(
+          (song) => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``
+        )
+        .join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`
+    );
+  })
+  // DisTubeOptions.searchSongs = true
+  .on("searchCancel", (message) => message.channel.send(`Searching canceled`))
+  .on("error", (message, e) => {
+    console.error(e);
+    message.channel.send("An error encountered: " + e);
+  });
